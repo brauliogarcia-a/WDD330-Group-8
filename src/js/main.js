947fc2c7cdb0bc54dd2f71832a74df26262b48bb
brauliogarcia-a <braulio.garcia.analyst@gmail.com>
2026-02-07 10:29:46 -0600

    Activity 4 - Week5

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import productList from "./productList.mjs";
 import { updateCartCount } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+loadHeaderFooter();
 
 productList(".product-list", "tents");
 updateCartCount();
