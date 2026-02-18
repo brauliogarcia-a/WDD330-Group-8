4b34a472c57b41cd7970ac18277f3a7de201f8c6
brauliogarcia-a <braulio.garcia.analyst@gmail.com>
2026-02-17 23:15:59 -0600

    Fix Netlify Render

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -96,27 +96,27 @@ export async function renderWithTemplate(
 }
 
 // load a template from a file
-function loadTemplate(path){
-  // wait what?  we are returning a new function? 
-  // this is called currying and can be very helpful.
-  return async function () {
-    const res = await fetch(path);
-    if (res.ok) {
-      const html = await res.text();
-      return html;
-    }
-  };
-}
+const partials = import.meta.glob("../partials/*.html", {
+  as: "raw",
+  eager: true,
+});
+
 
 // load header and footer
 export async function loadHeaderFooter() {
-  // header template will still be a function! But one where we have pre-supplied the argument.
-  // headerTemplate and footerTemplate will be almost identical, but they will remember the path we passed in when we created them
-  // why is it important that they stay functions?  The renderWithTemplate function is expecting a template function...if we sent it a string it would break, if we changed it to expect a string then it would become less flexible.
-  const headerTemplateFn = loadTemplate("../partials/header.html");
-  const footerTemplateFn = loadTemplate("../partials/footer.html");
+  const headerHTML = partials["../partials/header.html"];
+  const footerHTML = partials["../partials/footer.html"];
+
   const headerEl = document.querySelector("#main-header");
   const footerEl = document.querySelector("#main-footer");
-  await renderWithTemplate(headerTemplateFn, headerEl, {}, updateCartCount);
-  await renderWithTemplate(footerTemplateFn, footerEl);
+
+  if (headerEl) {
+    headerEl.innerHTML = headerHTML;
+  }
+
+  if (footerEl) {
+    footerEl.innerHTML = footerHTML;
+  }
+
+  updateCartCount();
 }
